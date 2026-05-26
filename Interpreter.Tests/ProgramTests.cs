1e2d8e63b41033f64e59d1bbaa12401e27cdb420
juner <juner@juner.net>
2026-05-26 17:51:22 +0900

    fix(piet-interpreter): restore original test and add entrypoint test

--- Interpreter.Tests/ProgramTests.cs
+++ Interpreter.Tests/ProgramTests.cs
@@ -1,11 +1,18 @@
+using Esolang.Piet.Interpreter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Reflection;
 
 namespace Esolang.Piet.Interpreter.Tests;
 
 [TestClass]
 public class ProgramTests
 {
+    [TestMethod]
+    public async Task RunAsync_Default_ReturnsZero()
+    {
+        var exitCode = await Program.RunAsync(Array.Empty<string>());
+        Assert.AreEqual(0, exitCode);
+    }
+
     [TestMethod]
     public void EntryPoint_Invoke_ReturnsZero()
     {
